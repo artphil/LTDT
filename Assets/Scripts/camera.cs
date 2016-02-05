1cc112fd645a5a02a1330e48ede4abcae01c2bb0
Arthur Phillip Silva <artphil7@yahoo.com.br>
2016-02-05 14:44:59 -0200

    Inicio - Primeira implementações incluindo movimentação do personagem.

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	public Transform player;
+	public float smooth = 0.5f;
+	public float velocity;
+	public float posicX;
+	public float posicY;
+	private Vector2 velocity2D;
+	private Vector2 newPosition2D = Vector2.zero;
+	private Vector3 newPosition = Vector3.zero; 
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		velocity2D = new Vector2 (velocity, velocity);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Recupera a posiçao do personagem e amortiza a volocidade da camera
+		newPosition2D.x = Mathf.SmoothDamp (transform.position.x, player.position.x, ref velocity2D.x, smooth);
+		newPosition2D.y = Mathf.SmoothDamp (transform.position.y, player.position.y, ref velocity2D.y, smooth);
+
+		// Verifica se ha movimento em X que nao ultrapassa a tela
+		if (newPosition2D.x > -posicX && newPosition2D.x < posicX)
+		{
+			newPosition = new Vector3 (newPosition2D.x, newPosition.y, transform.position.z);
+		}
+
+		// Verifica se ha movimento em X que nao ultrapassa a tela
+		if (newPosition2D.y > -posicY && newPosition2D.y < posicY) 
+		{
+			newPosition = new Vector3 (newPosition.x, newPosition2D.y, transform.position.z);
+		}
+
+		// Atualiza a posiçao da camera
+		//newPosition = new Vector3 (newPosition2D.x, newPosition2D.y, transform.position.z);
+
+		transform.position = Vector3.Slerp (transform.position, newPosition, Time.time);
+
+	}
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	//Movimento
+	public  float       velocity;
+	public	int			lado;
+
+	// Animacao
+	public  Transform   character;          //Personagem jogavel
+	private Animator    anime;              //Animacao do personagem
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Recupera a animacao do personagem
+		anime = character.GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{		
+		// Controla movimento
+		Move ();
+	}
+	
+	// Controla movimento 
+	void Move () {
+
+		// Verifica a seta é solta
+		if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) 
+			|| Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
+			lado = 0;
+		}
+
+		// Verifica se seta direita e pressionada
+		if (lado == 0 && Input.GetKeyDown(KeyCode.UpArrow)){
+			lado = 1;
+			anime.SetInteger ("lado", lado);			
+		}
+		// Verifica se seta baixo e pressionada
+		if (lado == 0 && Input.GetKeyDown(KeyCode.DownArrow)){
+			lado = 2;
+			anime.SetInteger ("lado", lado);
+		}
+		// Verifica se seta esquerda e pressionada
+		if (lado == 0 && Input.GetKeyDown(KeyCode.LeftArrow)){
+			lado = 3;
+			anime.SetInteger ("lado", lado);
+			transform.eulerAngles = new Vector2 (0, 0);
+		}
+		// Verifica se seta direita e pressionada
+		if (lado == 0 && Input.GetKeyDown(KeyCode.RightArrow)){
+				lado = 4;
+				anime.SetInteger ("lado", lado);
+				transform.eulerAngles = new Vector2 (0, 180);
+		}
+
+		// Move o personagem
+		if (lado == 1) {
+			transform.Translate (Vector2.up * velocity * Time.deltaTime);
+		} else if (lado == 2) {
+			transform.Translate (Vector2.up * -velocity * Time.deltaTime);
+		} else if (lado >= 3) {
+			transform.Translate (Vector2.right * -velocity * Time.deltaTime);
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D colisor) {
+	
+	}
+	
+}
